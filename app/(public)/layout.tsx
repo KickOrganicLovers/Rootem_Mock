8c61853337d8c52a3769fe7488da2b29f9ca44c3
Feet_Up <ukyo21owls@icloud.com>
2026-02-02 06:07:42 +0900

    ディレクトリの再構成とglobals.cssのインポートエラーの修正

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,10 +1,9 @@
 //共通の処理が必要な場合はここにかく
 
-
 export default function AuthRootLayout({ children }: { children: React.ReactNode }) {
-  return (
-      <div>
-          {children}
-      </div>
-  );
+    return (
+        <div>
+            {children}
+        </div>
+    );
 }
