6042bfe3168e5b1ca3e1cca863b759ffed7bf411
Feet_Up <ukyo21owls@icloud.com>
2026-02-05 17:12:03 +0900

    モック更新用のコミット5

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/Routem_Mock',
+  assetPrefix: '/Routem_Mock/',
   images: {
     unoptimized: true,
   },
